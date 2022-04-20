8a9039e878fcc51d1d97cc028a324563ad7d6e53
DavidIsege <isegedavid@gmail.com>
2022-04-20 13:20:50 +0100

    me50

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+
+    string name = get_string("What's your name? ");make
+    printf("hello, %s\n", name);
+}
